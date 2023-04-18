36ec98624ad6091405047f97b6859a496c040b36
mwangiwambui <wambui54mwangi@gmail.com>
2023-04-18 13:12:54 +0300

    Created the functions to get token and session for spotify tokens

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const {data:session} = useSession()
+  // console.log(session.user.accessToken)
+
+  const[x, setX] = useState('')
+
+  useEffect(()=>{
+    if(session && session.user && session.user.accessToken)
+    setX(session.user.accessToken)
+  },[session])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
